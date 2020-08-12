7a07f6ffbd633a4e9a9d4dcf46d99f05b5d4f77e
kvxmmu <kvx@pohui.org>
2020-08-12 05:05:32 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
+from os.path import dirname, realpath, join
+
+dir_ = dirname(realpath(__file__))
+deps_file = join(dir_, 'requirements.txt')
+
+with open(deps_file) as file:
+    deps = file.read().splitlines()
 
 setup(
     name='awtg',
     version='0.1',
     packages=find_packages(),
+    install_requires=deps
 )
